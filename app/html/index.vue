992692fc2790d2793ccdd53256136e7a3d802ca5
yp <py_yp@sina.com>
2017-04-07 15:57:25 +0800

    init

---
+++ app/html/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <header-component/>
+        <div>xkz</div>
+        <div>sb</div>
+        <other-component/>
+    </div>
+</template>
+<style>
+    body{
+        background-color:#ff0000;
+    }
+</style>
+<script>
+    import HeaderComponent from './components/header.vue'
+    import OtherComponent from './components/other.vue'
+    export default{
+        data(){
+            return{
+                msg:'hello vue'
+            }
+        },
+        components:{
+            'other-component':OtherComponent,
+            HeaderComponent,
+        }
+    }
+</script>
