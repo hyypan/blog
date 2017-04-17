a0ce26b086b489b98412be52a746618643a22833
yp <py_yp@sina.com>
2017-04-17 12:06:32 +0800

    css

--- app/yafish/src/App.vue
+++ app/yafish/src/App.vue
@@ -18,22 +18,27 @@ html, body{
   height:100%;
   margin:0;
   padding:0;
+
 }
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
     height:100%;
+    background-image: url('assets/bg.jpg');
+
 
 }
 .navi{
+    display:none;
     width: 100%;
-    height: 15%;
+    height: 10%;
     background: burlywood;
 }
 .footer{
+    display:none;
     width: 100%;
-    height:25%;
+    height:10%;
     background: black;
 }
 
--- app/yafish/src/components/Hello.vue
+++ app/yafish/src/components/Hello.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="content">content</div>
+  <div class="content">
+    <div class="list">这个地方放目录</div>
+    <div class="listRight">这个地方是目录显示出来的内容</div>
+  </div>
 </template>
 
 <script>
@@ -19,7 +22,28 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .content{
-height:60%;
+    height:80%;
 }
+.list{
+    height: 100%;
+    width: 20%;
+    background: beige;
+    border-right: 1px solid silver;
+    position: relative;
+    float: left;
+    margin-top:6%;
+}
+.listRight{
+    background: rebeccapurple;
+    width: 76%;
+    position: relative;
+    float: left;
+    margin: 10 auto;
+    top: 10px;
+    left: 10px;
+    height: 95%;
+    margin-top:6%;
+}
+
 
 </style>
