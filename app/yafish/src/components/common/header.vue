7f9a6f88e171b1fd2f3026f0094167586a94b4f5
yp <py_yp@sina.com>
2017-04-24 18:49:17 +0800

    input animate

--- app/yafish/src/components/common/header.vue
+++ app/yafish/src/components/common/header.vue
@@ -8,8 +8,8 @@
       <li>
         <div class="secondLine">
           <span class="inputLeft">搜索</span>
-          <input class="search"/>
-          <img class="inputReft" src="../../assets/search.png"></img>
+          <input class="search" @click="inputClick"/>
+          <div class="inputReft"><img src="../../assets/search.png"></img></div>
         </div>
       </li>
 
@@ -33,7 +33,7 @@ ul{
 ul li{
     margin-left: 50px;
     font-size: 23px;
-    margin-top: 37px;
+    margin-top: 20px;
 }
 .inputLeft{
     font-size: 10px;
@@ -41,9 +41,9 @@ ul li{
     width: 25px;
     display: block;
     background: white;
-    border: 1px solid rebeccapurple;
+    border: 1px solid #c1c1c1;
     padding-top: 14px;
-    margin-top: -17px;
+    margin-top: -4px;
     margin-left: 123px;
     border-radius: no;
     border-right: none;
@@ -56,15 +56,29 @@ ul li{
     margin-left: 153px;
     margin-top: -42px;
     height: 38px;
-    border: 1px solid rebeccapurple;
+    border: 1px solid #c1c1c1;
+    border-left:none;
+    border-right: none;
+}
+.search:focus{
+    //outline: -webkit-focus-ring-color auto 1px;
+    outline-color: #c1c1c1;
+    outline-style: none;
+    outline-width: 1px;
+
+}
+.inputReft{
+    height: 40px;
+    margin-left: 281px;
+    margin-top: -42px;
+    display: block;
+    border: 1px solid #c1c1c1;
+    border-left: none;
     border-bottom-right-radius: 23px;
     border-top-right-radius: 23px;
 }
-.inputReft{
-    width: 25px;
-    margin-left: 250px;
-    margin-top: -33px;
-    display: block;
+.inputReft img{
+  width:36px;
 }
 </style>
 <script>
@@ -74,6 +88,26 @@ ul li{
                 msg:'hello vue'
             }
         },
+        mounted:function(){
+        document.getElementsByClassName('search')[0].addEventListener('focusout',function(){
+
+        $('.inputReft img').animate({'margin-left':'0px'})
+        $('.inputReft').animate({'width':'40px'})
+        })
+
+        },
+
+
+        methods:{
+        inputClick:function(){
+        //document.getElementsByClassName('inputReft')[0].style.width='71px';
+        $('.inputReft img').animate({'margin-left':'30px'})
+        $('.inputReft').animate({'width':'71px'})
+        },
+        blur:function(){
+        alert(1)
+        }
+        }
 
     }
 </script>
