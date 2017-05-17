1c5c97b4218b060ca36d98ed0a42632119eeb5bf
yp <py_yp@sina.com>
2017-05-17 14:46:25 +0800

    mod mode

--- boke/yafish/admin.py
+++ boke/yafish/admin.py
@@ -7,3 +7,8 @@ from yafish import models as m
 @admin.register(m.Banner)
 class BannerAdmin(admin.ModelAdmin):
     list_display = ('name', 'banner_img', 'seq')
+
+
+@admin.register(m.Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'second_title', 'author', 'icon', 'content', 'read_times', 'good_click', 'plate')
--- boke/yafish/models.py
+++ boke/yafish/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.core.files.storage import FileSystemStorage
 from model_utils import models as util_models
 from django.db import models
+from redactor.fields import RedactorField
 
 # Create your models here.
 
@@ -43,11 +44,10 @@ class Plate(util_models.TimeStampedModel):
 
 class Article(util_models.TimeStampedModel):
     title = models.CharField(verbose_name='文章标题', max_length=200, help_text='字数不超过200字')
-    second_title = models.CharField(verbose_name='文章副标题', max_length=200, help_text='字数200字以下')
+    second_title = models.CharField(verbose_name='文章副标题', max_length=200, blank=True, null=True, help_text='字数200字以下')
     author = models.CharField(verbose_name='作者', max_length=50, help_text='中英文都可以')
-    # artice_type = models.ForeignKey(ArticleType, verbose_name='文章类型')
-    icon = models.ImageField(verbose_name='文章相关图片')
-    content = models.TextField(verbose_name='文章内容')
+    icon = models.ImageField(verbose_name='文章相关图片', blank=True, null=True)
+    content = RedactorField(verbose_name='文章内容')
     read_times = models.IntegerField(verbose_name='阅读数', default=0)
     good_click = models.IntegerField(verbose_name='点赞次数', default=0)
     plate = models.ForeignKey(Plate, verbose_name='文章所属板块')
